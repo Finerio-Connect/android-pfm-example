795fc2e0bb4881c888d448e10dd434c6eaab07e0
Erick-dominguez <erick@finerioconnect.com>
2022-04-04 12:53:20 -0500

    added local categories to show summary list

--- app/src/main/java/com/finerioconnect/pfm/example/mappers/SummaryMapper.kt
+++ app/src/main/java/com/finerioconnect/pfm/example/mappers/SummaryMapper.kt
@@ -22,9 +22,9 @@ fun getTransactions(resumesByMonth: List<FCResumeByMonth>, isNegativeAmount: Boo
 }
 
 fun getTransaction(resumeByMonth: FCResumeByMonth, resumeByCategory: FCResumeByCategory, isNegativeAmount: Boolean, categories: List<FCCategory>): FCResumeTransaction {
-    val imageName = "https://cdn-icons.flaticon.com/png/512/1078/premium/1078443.png?token=exp=1646424932~hmac=cf442ef4a3be4abab5a70e1e7132aa71"
-    val amount = resumeByCategory.amount
     val category: FCCategory? = getCategoryById(resumeByCategory.categoryId, categories)
+    val imageName = getImageName(category)
+    val amount = resumeByCategory.amount
     val image = FCImage(imageName, Color.parseColor(category?.color?:"#ff939598"))
     return FCResumeTransaction(
         imageName,
@@ -37,3 +37,24 @@ fun getTransaction(resumeByMonth: FCResumeByMonth, resumeByCategory: FCResumeByC
 
 fun getCategoryById(categoryId: Int, categories: List<FCCategory>): FCCategory? =
     categories.find { it.id == categoryId }
+
+fun getImageName(category: FCCategory?) : String {
+    var imageName = "https://cdn-icons.flaticon.com/png/512/1078/premium/1078443.png?token=exp=1646424932~hmac=cf442ef4a3be4abab5a70e1e7132aa71"
+    if (category != null) {
+        imageName = when (category.name) {
+            "Transacciones Bancarias" -> "00000000_0000_0000_0000_000000000047"
+            "Finanzas e Impuestos" -> "00000000_0000_0000_0000_00000000000b"
+            "Hogar" -> "00000000_0000_0000_0000_000000000001"
+            "Entretenimiento" -> "00000000_0000_0000_0000_000000000003"
+            "Alimentos" -> "00000000_0000_0000_0000_000000000002"
+            "Regalos y Ayuda" -> "00000000_0000_0000_0000_000000000008"
+            "Salud y Belleza" -> "00000000_0000_0000_0000_000000000005"
+            "Educación y Trabajo" -> "00000000_0000_0000_0000_000000000007"
+            "Auto y Transporte" -> "00000000_0000_0000_0000_000000000006"
+            "Ingresos" -> "00000000_0000_0000_0000_000000000050"
+            else -> "https://cdn-icons.flaticon.com/png/512/1078/premium/1078443.png?token=exp=1646424932~hmac=cf442ef4a3be4abab5a70e1e7132aa71"
+        }
+    }
+
+    return imageName
+}
